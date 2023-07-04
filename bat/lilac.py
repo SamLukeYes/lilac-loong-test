0ca4e7df5c4dba65212bbaca49b3ec1de0c14e80
SamLukeYes <samlukeyes123@gmail.com>
2023-07-04 21:04:31 +0800

    repo test

---
+++ bat/lilac.py
@@ -0,0 +1,14 @@
+from types import SimpleNamespace
+
+from lilaclib import *
+
+g = SimpleNamespace()
+
+def pre_build():
+    g.files = download_official_pkgbuild('bat')
+    add_into_array('arch', ['loong64'])
+
+def post_build():
+    git_add_files(g.files)
+    git_commit()
+    _run_cmd(['cp' '*.pkg.tar*' '~/repo/extra/spool'])
