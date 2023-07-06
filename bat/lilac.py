b2ebfef8606d2105363454f4ff6f5878849d06b0
SamLukeYes <samlukeyes123@gmail.com>
2023-07-06 12:58:51 +0800

    Test destsubdir

--- bat/lilac.py
+++ bat/lilac.py
@@ -7,6 +7,12 @@ g = SimpleNamespace()
 def pre_build():
     g.files = download_official_pkgbuild('bat')
     add_into_array('arch', ['loong64'])
+    for line in edit_file('PKGBUILD'):
+        if line.lstrip().startswith('cargo fetch --locked'):
+            line = line.replace(
+                'cargo fetch --locked', 'cargo update'
+            )
+        print(line)
 
 def post_build():
     git_add_files(g.files)
